e8fefd135b7f061601a48c5e4a1d9eea6b732b2d
Ryan LaBarge <ryanclabarge@gmail.com>
2017-11-20 15:45:04 -0500

    changed to tweepy

--- twitter_test.py
+++ twitter_test.py
@@ -1,6 +1,19 @@
-from twython import Twython
+import tweepy
 import settings
 
-twitter = Twython(settings.APP_KEY,access_token=settings.ACCESS_TOKEN)
+auth = tweepy.OAuthHandler(settings.consumer_key,settings.consumer_secret)
 
-print(twitter.search(q='python'))
+auth.set_access_token(settings.access_token,settings.access_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.user_timeline(screen_name='arlabarge',count=2)
+for tweet in public_tweets:
+    print(tweet.text)
+
+user = api.get_user('arlabarge')
+for friend in user.friends():
+    print(friend.screen_name)
+
+#api.send_direct_message(screen_name='arlabarge', text='Direct Message')
+
