730ffb184a1d247dfc4ec94e4e3145665319f74b
Ryan LaBarge <ryanclabarge@gmail.com>
2017-11-27 10:47:21 -0500

    added hashtag and user mentions

--- twitter_test.py
+++ twitter_test.py
@@ -7,13 +7,23 @@ auth.set_access_token(settings.access_token,settings.access_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.user_timeline(screen_name='arlabarge',count=1)
+public_tweets = api.user_timeline() #this takes the tweets and pulls out all the hashtags and user mentions
 for tweet in public_tweets:
-    print(tweet.text)
+    hash = tweet.entities.get('hashtags')
+    mention = tweet.entities.get('user_mentions')
+    #print(tweet.text)
+    for hashtag in hash:  #prints all hashtags
+        print(hashtag.get('text'))
+    for mentions in mention: #prints all users mentioned
+        print(mentions.get('name'))
 
-user = api.get_user('arlabarge')
-for friend in user.friends():
-    print(friend.screen_name)
+# user = api.get_user('arlabarge')
+# for friend in user.friends():
+#     print(friend.screen_name)
+
+# my_current_status = api.user_timeline(count=1)
+# for tweet in my_current_status:
+#     print(tweet.text)
 
 #api.send_direct_message(screen_name='arlabarge', text='Direct Message')
 
